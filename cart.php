ac9161e17d4fc1a6f59bdc86992830bf8f249831
Yassine Lafryhi <y.lafryhi@gmail.com>
2021-06-22 05:41:45 +0100

    Create cart.php

---
+++ cart.php
@@ -0,0 +1,98 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+
+<html lang="en" class="perfect-scrollbar-off">
+<head>
+    <meta charset="utf-8">
+    <title>
+       Cart elements
+    </title>
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no"
+          name="viewport">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+<body class="user-profile">
+<div class="wrapper ">
+    <div class="main-panel" id="main-panel">
+        <div class="content">
+            <center>
+                <button onclick="location.href='index.php'" class="btn btn-info btn-round"
+                        type="button">Home Page
+                </button>
+            </center>
+            <br>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 style="text-align: center" class="card-title">Cart products</h4>
+                        </div>
+                        <div class="card-body">
+                            <div style="text-align: center" class="col-md-12">
+                                    <button onclick="logout()" class="btn btn-success btn-round"
+                                            type="button">Check out
+                                    </button>
+                            </div>
+                            <br>
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead class=" text-primary">
+                                    <tr style='text-align: center'>
+                                        <th>
+                                            Id
+                                        </th>
+                                        <th>
+                                            Product
+                                        </th>
+                                        <th>
+                                            Price
+                                        </th>
+                                        <th>
+                                           Quantity
+                                        </th>
+                                        <th>
+                                            Total
+                                        </th>
+                                        <th>
+                                            Actions
+                                        </th>
+                                    </tr>
+                                    </thead>
+                                    <tbody id="list">
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="assets/js/main.js"></script>
+
+<script>
+    getCartElements();
+</script>
+
+
+</body>
+</html>
+
